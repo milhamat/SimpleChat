7af40f291a3af36438b7eb8d52a116fcc1b1b278
MilhamAt <M477ashiddiq@gmail.com>
2020-10-17 20:09:58 +0700

    chatBrain_getSting_firstLogic

--- Model/ChatBrain.swift
+++ Model/ChatBrain.swift
@@ -13,4 +13,19 @@ struct ChatBrain {
         Chat(bot: "Apa Kabar", person: ""),
         Chat(bot: "", person: "Baik ")
     ]
+    
+    func getString() -> String{
+        var chat = ""
+        for i in 0...2{
+            if chatArray[i].bot != "" && chatArray[i].person .isEmpty{
+                chat = chatArray[i].bot
+            }
+            if chatArray[i].bot .isEmpty && chatArray[i].person != "" {
+                chat = chatArray[i].person
+            }
+        }
+        return chat
+    }
+
 }
+
